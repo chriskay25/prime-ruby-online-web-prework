76a609dd613a9a2d1d5dba911638d8d48aa74e00
Chris Kay <chriskay25@gmail.com>
2019-04-13 18:34:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
-
-
 def prime?(num)
-  
+primes = [2,3,5,7,11,13,17,19,23,29]
+  return false if num < 2 
+  return true if num == 3 || num == 2 
+    primes.each do |i| 
+      if num % i == 0
+        return false
+      else num % i != 0
+        return true
+    end
+    end
 end
